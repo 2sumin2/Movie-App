6d034d22af8087255f9bc96418ddd164d194e676
sumin <lsm001005@naver.com>
2022-03-14 15:44:38 +0900

    second commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
     return (
         <div>
             <img src={coverImg} alt={title} />
-            <h2>{title}</h2>
-            <p>{summary}</p>
+            <h2>
+                <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map((g) => (
                     <li key={g}>{g}</li>
@@ -14,6 +17,7 @@ function Movie({ coverImg, title, summary, genres }) {
         </div>);
 }
 Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     coverImge: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
